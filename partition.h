0a320d47ddfd821ea64dc293c814aec35462635c
Emilija <emilija.rimselyte@gmail.com>
2025-03-26 09:50:49 +0200

    Update partition.h

--- partition.h
+++ partition.h
@@ -1,20 +1,20 @@
 #ifndef PARTITION_H
 #define PARTITION_H
 
-#define MAX_ITEMS 1000     // Maximum number of items
-#define MAX_CAPACITY 10000 // Maximum capacity
+#define MAX_ITEMS 100
+#define MAX_CAPACITY 10000
 
 typedef struct {
-    int subset1[MAX_ITEMS];
-    int subset2[MAX_ITEMS];
-    int sum1;
-    int sum2;
-    int difference;
+    int sum1;           // Sum of first subset
+    int sum2;           // Sum of second subset
+    int difference;     // Difference between subset sums
+    int subset1[MAX_ITEMS];  // Elements in first subset
+    int subset2[MAX_ITEMS];  // Elements in second subset
+    int subset1_size;   // Number of elements in first subset
+    int subset2_size;   // Number of elements in second subset
 } PartitionResult;
 
-void read_input(const char *filename, int *weights, int *values, int *n, int *capacity, int *is_partition);
 PartitionResult knapsack_or_partition(int *weights, int *values, int n, int capacity, int is_partition);
-int read_input_stdin(int *weights, int *values, int *n, int *capacity, int *is_partition);
-void run_test_with_params(int *weights, int *values, int n, int capacity, int is_partition, const char *mode, int timeout);
+void read_input(const char *filename, int *weights, int *values, int *n, int *capacity, int *is_partition);
 
 #endif
